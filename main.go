7e2f437c89099cdb3037f9cc6338b45b3372a55e
Sajjad Ahmadi <programmer1404@gmail.com>
2025-03-07 13:33:55 +0330

    Bump versionInfo to 0.20

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ const (
 	defaultPort        = "8443"
 	updateScriptURL    = "https://k4m.me/bot/gopv.sh"
 	updateScriptPath   = "/root/gopv.sh"
-	versionInfo        = "0.19"
+	versionInfo        = "0.20"
 )
 
 // Global variables
